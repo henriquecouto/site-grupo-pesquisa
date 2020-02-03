64fd61fedbc551a7dfc289d45dac1f4aee3d903b
Henrique Couto <henrique.coutu@live.com>
2020-02-02 23:02:29 -0300

    showing news

--- src/screens/News/index.js
+++ src/screens/News/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import logo from "../../assets/logo.png";
-import { loadScreen } from "../../services/firebase/db";
+import { loadScreen, loadNews } from "../../services/firebase/db";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,11 +18,43 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     width: "100%"
+  },
+  paper: {
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(0)
+    // height: 300
+  },
+  gridNews: {
+    width: "100%",
+    height: "100%",
+    overflowY: "auto",
+    "&::-webkit-scrollbar": {
+      width: 5
+    },
+
+    "&::-webkit-scrollbar-track": {
+      background: theme.palette.primary.main
+    },
+
+    "&::-webkit-scrollbar-thumb": {
+      background: "#888"
+    },
+
+    "&::-webkit-scrollbar-thumb:hover": {
+      background: "#555"
+    }
   }
 }));
 
 export default function News({ screen }) {
   const classes = useStyles();
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = loadNews(setNews, { limit: 6 });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <Grid
@@ -32,12 +64,51 @@ export default function News({ screen }) {
       alignItems="flex-start"
     >
       <Grid item xs={8}>
-        <Typography variant="h2" color="textPrimary" className={classes.title}>
-          {screen.name}
-        </Typography>
-        <Typography variant="h6" color="textPrimary">
-          {screen.content}
-        </Typography>
+        <Grid container direction="column" spacing={9}>
+          <Grid item>
+            <Typography
+              variant="h2"
+              color="textPrimary"
+              className={classes.title}
+            >
+              {screen.name}
+            </Typography>
+            <Typography variant="h6" color="textPrimary">
+              {screen.content}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <div style={{ height: 400 }}>
+              <Grid container className={classes.gridNews} spacing={2}>
+                {[
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news,
+                  ...news
+                ].map(notice => (
+                  <Grid item key={notice.id} xs={4}>
+                    <Paper className={classes.paper}>
+                      <div style={{ padding: "10px 20px" }}>
+                        <Typography variant="h6">{notice.title}</Typography>
+                        <Typography variant="body1">{notice.desc}</Typography>
+                      </div>
+                    </Paper>
+                  </Grid>
+                ))}
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
       </Grid>
       <Grid item xs={3} className={classes.right}>
         <img src={screen.image} className={classes.logo} alt="" />
--- src/services/firebase/db.js
+++ src/services/firebase/db.js
@@ -1,13 +1,15 @@
 import { db } from "./config";
 
+const onSnapshot = (snapshot, callback) => {
+  const result = snapshot.docs.map(v => ({ id: v.id, ...v.data() }));
+  return callback(result);
+};
+
 export const loadScreens = callback => {
   const unsubscribe = db
     .collection("screens")
     .orderBy("id", "asc")
-    .onSnapshot(snapshot => {
-      const result = snapshot.docs.map(v => ({ id: v.id, ...v.data() }));
-      callback(result);
-    });
+    .onSnapshot(snapshot => onSnapshot(snapshot, callback));
   return unsubscribe;
 };
 
@@ -16,9 +18,14 @@ export const loadMembers = callback => {
     .collection("members")
     .orderBy("first", "desc")
     .orderBy("name", "desc")
-    .onSnapshot(snapshot => {
-      const result = snapshot.docs.map(v => ({ id: v.id, ...v.data() }));
-      callback(result);
-    });
+    .onSnapshot(snapshot => onSnapshot(snapshot, callback));
+  return unsubscribe;
+};
+
+export const loadNews = (callback, { limit }) => {
+  const unsubscribe = db
+    .collection("news")
+    .orderBy("date", "asc")
+    .onSnapshot(snapshot => onSnapshot(snapshot, callback));
   return unsubscribe;
 };
